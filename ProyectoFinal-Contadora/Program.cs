2722cb0b579165e5f4f2dbdc1d268607eee84f51
steffi lude <m.houdini86@gmail.com>
2024-10-31 18:52:53 -0300

    comentario en program.cs

--- ProyectoFinal-Contadora/Program.cs
+++ ProyectoFinal-Contadora/Program.cs
@@ -16,7 +16,7 @@ namespace ProyectoFinal_Contadora
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmLogin());
+            Application.Run(new frmLogin()); //inicio
         }
     }
 }
